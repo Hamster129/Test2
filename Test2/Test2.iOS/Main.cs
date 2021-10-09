d76576505384738dd3675837356aae0d936476a9
Hamster129 <pavliy2045@mail.ru>
2021-10-09 19:49:40 +0300

    - Coments

--- Test2/Test2.iOS/Main.cs
+++ Test2/Test2.iOS/Main.cs
@@ -9,11 +9,10 @@ namespace Test2.iOS
 {
     public class Application
     {
-        // This is the main entry point of the application.
+        
         static void Main(string[] args)
         {
-            // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
+          
             UIApplication.Main(args, null, "AppDelegate");
         }
     }
